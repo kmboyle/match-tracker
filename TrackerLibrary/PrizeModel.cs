dcf5937eac851d5083da1c36029cbd473721c4c3
Kevin Boyle <kevin.michael.boyle@gmail.com>
2024-01-16 13:44:36 -0500

    start setup

---
+++ TrackerLibrary/PrizeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace TrackerLibrary
+{
+	public class PrizeModel
+	{
+		public int Placeholder { get; set; }
+		public string PlaceName { get; set; }
+		public decimal PrizeAmount { get; set; }
+		public double PrizePercentage { get; set; }
+
+		public PrizeModel()
+		{
+		}
+	}
+}
+
